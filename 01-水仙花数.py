db78701f9a1371dd39780c87d54a9d54d65f228f
xiaoxiaoqiandao <xiaoxiaoqiandao@gmail.com>
2019-06-19 00:42:01 +0800

    水仙花数

--- 01-水仙花数.py
+++ 01-水仙花数.py
@@ -1,6 +1,7 @@
+//在数论中，水仙花数（Narcissistic number）用来描述一个N位非负整数，其各位数字的N次方和等于该数本身。
+
 print ('please input number')
-num = input()
-int_num = int(num)
+num = input() int_num = int(num)
 sum_num = 0
 for i in num:
     sum_num += int(i)**len(num)
